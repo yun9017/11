9554378119d9fce0fe7bb2ac52b6f55a1c5d66b7
yun9017 <yun9017ie@naver.com>
2025-11-16 21:20:21 +0900

    2

--- 11/11/main.c
+++ 11/11/main.c
@@ -8,19 +8,11 @@
 #include <stdio.h>
 
 int main(void) {
-    int i = 10;
-    char c = 'a';
-    int *iptr;
-    char *cptr;
-    int *iptr2;
+    int i = 300;
     
-    iptr = &i;
-    cptr = &c;
-    iptr2 = iptr;
+    int *pi = &i;
+    char *pc = &i;
     
-    printf("i:%p\n%p(size:%i)\n", iptr, &i, sizeof(iptr));
-    printf("c:%p\n%p(size:%i)\n", cptr, &c, sizeof(cptr));
-    printf("iptr2:%p, %i\n", iptr2, *iptr2);
-    
-    return 0;
+    printf("%i, %i, %i\n", i, *pi, *pc);
+
 }

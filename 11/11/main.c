c07212696d47bf633a6153b144cd4801421fa04b
yun9017 <yun9017ie@naver.com>
2025-11-16 21:51:45 +0900

    5

--- 11/11/main.c
+++ 11/11/main.c
@@ -8,23 +8,20 @@
 #include <stdio.h>
 
 int main(void) {
-    int i;
-    int grade[5];
-    int sum=0;
+    char *pc;
+    int *pi;
+    double *pd;
     
-    for(i=0;i<5;i++)
-    {
-        printf("grade[%i]= ", i);
-        scanf("%d", &grade[i]);
-    }
+    pc = (char*)10000;
+    pi = (int*)10000;
+    pd = (double*)10000;
     
-    for(i=0;i<5;i++)
-    {
-        printf("grade[%i] = %i\n", i, *(grade+i));
-        sum = sum + *(grade+i);
-    }
+    printf("증가 전: pc = %d, pi = %d, pd = %d\n", pc, pi, pd);
     
-    printf("Average : %i\n", sum/5);
+    pc++;
+    pi++;
+    pd++;
+    printf("증가 후: pc = %d, pi = %d, pd = %d\n", pc, pi, pd);
     
     return 0;
 }
